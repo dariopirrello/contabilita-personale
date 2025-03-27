3061536fcd7ef588d32ecfd503df044b3875a7df
DESKTOP-LJ2DHFE\Dario <dariopirrello.job@gmail.com>
2025-03-27 20:46:05 +0100

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'; // Importa HttpClientModule
 import { AppRoutingModule } from './app-routing.module';
+
+import { LoginComponent } from './component/login/login.component';
+
+
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  private apiUrl = 'http://localhost:8080/api/auth/login'; // URL backend corretto
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient, // Iniettiamo HttpClient
+    private router: Router    // Per reindirizzare dopo il login
+  ) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      console.log('Login con:', this.loginForm.value);
+
+      this.http.post<{ token: string }>(this.apiUrl, this.loginForm.value).subscribe(
+        (response) => {
+          console.log('Risposta API:', response);
+          localStorage.setItem('token', response.token);
+          this.router.navigate(['/dashboard']);
+        },
+        (error) => {
+          console.error('Errore nel login:', error);
+          alert('Credenziali non valide.');
+        }
+      );
+    }
+  }
+}
